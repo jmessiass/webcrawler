f340d94878df878320435c050d706c4eccde7c47
Jonathan Messias <jonathan.santos@lexisnexis.com>
2017-08-25 17:05:18 -0300

    change name from method

--- webcrawler.py
+++ webcrawler.py
@@ -9,7 +9,7 @@ print(42 * '-')
 print('\033[32m' + ' by Jonathan Messias | jmcybers@gmail.com' + '\033[0m')
 print(42 * '-')
 print('1. Write an URL   >>>>> http://target.com')
-print('2. Set any URLs   >>>>>>> /tmp/target.txt')
+# print('2. Set any URLs   >>>>>>> /tmp/target.txt')
 print(42 * '-')
 
 
@@ -17,9 +17,9 @@ def input_validation():
     """ validate input data """
     while True:
         try:
-            option = int(input('$ Choose an option [1-2]: '))
+            option = int(input('$ Choose an option [1]: '))
             print(42 * '-')
-            if option < 3:
+            if option < 2:
                 return option
             else:
                 print('\033[31m' + '$ %d is an invalid option !!!' % option + '\033[0m')
@@ -31,38 +31,26 @@ def input_validation():
             print(42 * '-')
 
 
-def choose_option(option):
+def set_target(option):
     """ choose an option and validade that """
     if option == 1:
-        text = 'URL: '
-        value = input('[{0}] {1}'.format(option, text))
-        print(42 * '-')
-        try:
-            page = urlopen(value)
-            return option, page
-        except(URLError, ValueError):
-            print('\033[33m' + '$ Error in your URL, try again !!!' + '\033[0m')
-            print(42 * '-')
-            choose_option(option)
-    elif option == 2:
-        print('\033[33m' + '[2] Sorry, this tool is not availabe =^(' + '\033[0m')
-        print(42 * '-')
-        # text = 'FILE: '
-
-    return input_validation()
+        while True:
+            try:
+                value = input('[{0}] URL: '.format(option))
+                print(42 * '-')
+                page = urlopen(value)
+                return option, page
+            except(URLError, ValueError):
+                print('\033[33m' + '$ Error in your URL, try again !!!' + '\033[0m')
+                print(42 * '-')
 
 
-def get_text(option, page):
+def extract_text(option, page):
     """ get all html text from target """
     if option == 1:
-        try:
-            html = page.read()
-        except URLError:
-            print('Error in your URL, try again !!!')
-            get_text()
-
-    bs_html = BeautifulSoup(html, 'lxml')
-    return get_words(bs_html)
+        html = page.read()
+        bs_html = BeautifulSoup(html, 'lxml')
+        return get_words(bs_html)
 
 
 def get_words(text):
@@ -97,5 +85,5 @@ def get_words(text):
 
 if __name__ == "__main__":
     option = input_validation()
-    option, page = choose_option(option)
-    print(get_text(option, page))
+    option, page = set_target(option)
+    print(extract_text(option, page))
